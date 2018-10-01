9801b59f3c98f173e959e0407874bcfbd1f3a902
Kelvin <kcampove@uncc.edu>
2018-10-01 22:43:07 +0000

    ready for heroku

--- app.rb
+++ app.rb
@@ -38,8 +38,16 @@ class HangpersonApp < Sinatra::Base
   # If a guess is repeated, set flash[:message] to "You have already used that letter."
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
-    letter = params[:guess].to_s[0]
+    letter = params[:guess].to_s
+      
     ### YOUR CODE HERE ###
+    begin
+        if @game.guess(letter) == false
+            flash[:message] = "You have already used that letter."
+        end
+    rescue ArgumentError
+        flash[:message] = "Invalid guess."
+    end
     redirect '/show'
   end
   
@@ -50,16 +58,27 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
+    if @game.check_win_or_lose == :win
+        redirect '/win'
+    elsif @game.check_win_or_lose == :lose
+        redirect '/lose'
+    end
     erb :show # You may change/remove this line
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
+    if @game.check_win_or_lose != :win
+        redirect '/show'
+    end
     erb :win # You may change/remove this line
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
+    if @game.check_win_or_lose != :lose
+        redirect '/show'
+    end
     erb :lose # You may change/remove this line
   end
   
--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,27 +1,59 @@
 class HangpersonGame
 
-  # add the necessary class methods, attributes, etc. here
-  # to make the tests in spec/hangperson_game_spec.rb pass.
-
-  # Get a word from remote "random word" service
-
-  # def initialize()
-  # end
+  attr_accessor :word 
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
   
   def initialize(word)
+ 
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    
   end
 
-  # You can test it by running $ bundle exec irb -I. -r app.rb
-  # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
-  #  => "cooking"   <-- some random word
+  def guess(input)
+    
+    if  input == nil or input == "" or !input.match(/[A-Za-z]/)  # regular expressions for characters a through z 
+      throw "Error"
+    end
+    
+    if  @guesses.downcase.include? input.downcase or  @wrong_guesses.downcase.include? input.downcase
+      return false
+    end
+    
+    if @word.downcase.include? input.downcase # validates the case sensitive 
+        @guesses += input
+    else
+        @wrong_guesses += input # wrong guesses plus input 
+    end
+  end
+  
+
+  
+  def word_with_guesses(word=@word, guesses=@guesses)
+    word.gsub(/[^ #{guesses}]/, '-') # Returns a copy of str with the all occurrences of pattern substituted for the second argument. 
+
+  end
+  
+  def check_win_or_lose
+    
+    if @wrong_guesses.length == 7
+      return :lose
+    end
+    
+    if not word_with_guesses.include? "-" # include the guesses including - 
+      return :win
+    end 
+    
+    return :play
+  end
+
   def self.get_random_word
     require 'uri'
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
-    Net::HTTP.new('watchout4snakes.com').start { |http|
-      return http.post(uri, "").body
-    }
+    Net::HTTP.post_form(uri ,{}).body
   end
 
 end
